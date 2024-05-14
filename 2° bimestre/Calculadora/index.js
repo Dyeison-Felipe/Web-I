773f599aa809784f877ecb2d2311cf0e9fe252f2
Dyeison-Felipe <dyeisonfc@outlook.com>
2024-05-13 22:07:51 -0300

    Atividades feita em sala

---
+++ 2° bimestre/Calculadora/index.js
@@ -0,0 +1,22 @@
+function calcular(tipo,valor){
+    console.log(tipo,valor)
+    if(tipo === 'simbolo'){
+        if (valor === 'c'){
+            document.getElementById('resultado').value = '';
+        }
+
+        if(valor === '+' || valor === '-' || valor === '*'|| valor === '/'|| valor === '.'){
+            document.getElementById('resultado').value += valor;
+        }
+
+        if(valor === '='){
+            let conta = eval(document.getElementById('resultado').value)
+
+            document.getElementById('resultado').value = conta;
+        }
+        
+    }else if(tipo === 'numero'){
+
+        document.getElementById('resultado').value += valor
+    }
+}
--- 2° bimestre/Exercícios_Js/atvd_03/index.js
+++ 2° bimestre/Exercícios_Js/atvd_03/index.js
@@ -1,31 +1,30 @@
 const form = document.getElementById('formNota')
 
 form.addEventListener('submit', function (evento) {
-    evento.preventDefault() // impede o envio do formulario sem que todos os campos sejam preenchidos
+  evento.preventDefault() // impede o envio do formulario sem que todos os campos sejam preenchidos
 
-    const nota1 = document.getElementById('nota').value
+  const nota1 = document.getElementById('nota').value
 
-    if (nota1 === '') {
-        document.querySelector('#resultado').innerHTML = "Todos os campos precisam ser preenchidos"
-        return false
+  if (nota1 === '') {
+    document.querySelector('#resultado').innerHTML = "Todos os campos precisam ser preenchidos"
+    return false
+  }
+
+
+  function resultadoNota() {
+
+    const convNota = Number(nota1)
+
+    if (convNota < 6 && convNota > 0) {
+      document.querySelector('#resultado').innerHTML = "Aluno REPROVADO"
+    } else if (convNota > 6 && convNota < 8) {
+      document.querySelector('#resultado').innerHTML = "Aluno APROVADO"
+    } else if (convNota >= 8) {
+      document.querySelector('#resultado').innerHTML = "Aluno APROVADO COM LOUVOR"
     }
+  }
 
-
-
-    function resultadoNota() {
-
-        const convNota = Number(nota1)
-
-        if (convNota < 6 && convNota > 0) {
-            document.querySelector('#resultado').innerHTML = "Aluno REPROVADO"
-        } else if (convNota > 6 && convNota < 8) {
-            document.querySelector('#resultado').innerHTML = "Aluno APROVADO"
-        } else if (convNota >= 8) {
-            document.querySelector('#resultado').innerHTML = "Aluno APROVADO COM LOUVOR"
-        }
-    }
-
-    resultadoNota()
+  resultadoNota()
 
 })
 
